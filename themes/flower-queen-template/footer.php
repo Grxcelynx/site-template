67f7f647529150041105dcd5f095d3f4cc3b3842
Grace Birnam <gbirnam@alphaworks.tech>
2023-02-17 15:07:41 -0800

    newn

--- themes/flower-queen-template/footer.php
+++ themes/flower-queen-template/footer.php
@@ -12,19 +12,18 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+		<!-- <div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'flower-queen-template' ) ); ?>">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'flower-queen-template' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'flower-queen-template' ), 'flower-queen-template', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
-		</div><!-- .site-info -->
+		</div> -->
+		<!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- themes/flower-queen-template/header.php
+++ themes/flower-queen-template/header.php
@@ -16,7 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Fira+Sans&family=Philosopher&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
@@ -27,12 +29,18 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
+			<?php the_custom_logo(); ?>
+			<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 			<?php
-			the_custom_logo(); ?>
+			$flower_queen_template_description = get_bloginfo( 'description', 'display' );
+			if ( $flower_queen_template_description || is_customize_preview() ) :
+				?>
+				<h5 class="site-description"><?php echo $flower_queen_template_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></h5>
+			<?php endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"> <i class="burger-menu fa-solid fa-bars"></i></button>
+		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="burger-menu"> &#9776;</span></button>
 			<?php
 			wp_nav_menu(
 				array(
